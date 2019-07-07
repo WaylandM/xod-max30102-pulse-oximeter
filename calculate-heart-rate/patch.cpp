d5f749cec4e261774f6707625a330603ef013a3d
Matt <mw283@cam.ac.uk>
2019-07-08 00:58:54 +0100

    modified:   calculate-heart-rate/patch.cpp 	modified:   calculate-heart-rate/patch.xodp 	modified:   heart-rate-monitor/patch.xodp

--- calculate-heart-rate/patch.cpp
+++ calculate-heart-rate/patch.cpp
@@ -1,9 +1,9 @@
 
 struct State {
-    const byte RATE_SIZE = 4; //Increase this for more averaging. 4 is good.
-    byte rates[RATE_SIZE]; //Array of heart rates
-    byte rateSpot = 0;
-    long lastBeat = 0; //Time at which the last beat occurred
+    const byte RATE_SIZE; //Increase this for more averaging. 4 is good.
+    uint8_t rates[RATE_SIZE]; //Array of heart rates
+    uint8_t rateSpot;
+    long lastBeat; //Time at which the last beat occurred
     float beatsPerMinute;
     int beatAvg;
 };
@@ -11,6 +11,42 @@ struct State {
 {{ GENERATED_CODE }}
 
 void evaluate(Context ctx) {
-    //auto inValue = getValue<input_IN>(ctx);
-    //emitValue<output_OUT>(ctx, inValue);
+    // The node responds only if there is an input pulse
+    if (!isInputDirty<input_UPD>(ctx))
+        return;
+
+    // get state
+    State* state = getState(ctx);
+
+    if (isSettingUp())
+    {
+        state->RATE_SIZE = 4;
+        state->rates[state->RATE_SIZE];
+        state->rateSpot = 0;
+        state->lastBeat = 0;
+    }
+
+    long irValue = getValue<input_IR>(ctx);
+    
+    if (checkForBeat(irValue) == true)
+    {
+        //We sensed a beat!
+        emitValue<output_BEAT>(ctx,1);
+        long delta = millis() - state->lastBeat;
+        state->lastBeat = millis();
+        state->beatsPerMinute = 60 / (delta / 1000.0);
+        if (state->beatsPerMinute < 255 && state->beatsPerMinute > 20)
+        {
+            state->rates[state->rateSpot++] = (uint8_t)state->beatsPerMinute; //Store this reading in the array
+            state->rateSpot %= state->RATE_SIZE; //Wrap variable
+            //Take average of readings
+            state->beatAvg = 0;
+            for (byte x = 0 ; x < state->RATE_SIZE ; x++)
+                state->beatAvg += state->rates[x];
+            state->beatAvg /= state->RATE_SIZE;
+            emitValue<output_BPM>(ctx,state->beatsPerMinute);
+            emitValue<output_AvgBPM>(ctx,state->beatAvg);
+        }
+    }
+    emitValue<output_DONE>(ctx,1);
 }
