b6a194fbed1032310ffec40fb373be7c1597d4ff
Matt <mw283@cam.ac.uk>
2019-07-07 10:19:19 +0100

    new file:   init/patch.xodp 	new file:   max30102-device/patch.cpp 	new file:   max30102-device/patch.xodp 	new file:   project.xod 	new file:   setup/patch.xodp

---
+++ max30102-device/patch.cpp
@@ -0,0 +1,34 @@
+// Tell XOD where it could download the library:
+#pragma XOD require "https://github.com/sparkfun/SparkFun_MAX3010x_Sensor_Library"
+
+//Include C++ libraries
+{{#global}}
+#include <Wire.h>
+#include <MAX30105.h>
+#include <spo2_algorithm.h>
+#include <heartRate.h>
+{{/global}}
+
+// Reserve memory to store an instance of the class,
+// and create the instance later:
+struct State {
+    uint8_t mem[sizeof(MAX30105)];
+};
+
+// Define our custom type as a pointer on the class instance.
+using Type = MAX30105*;
+
+{{ GENERATED_CODE }}
+
+void evaluate(Context ctx) {
+    // It should be evaluated only once on the first (setup) transaction
+    if (!isSettingUp())
+        return;
+
+    auto state = getState(ctx);
+
+    // Create a new object in the memory area reserved previously.
+    Type airSensor = new (state->mem) SCD30();
+
+    emitValue<output_DEV>(ctx, airSensor);
+}
